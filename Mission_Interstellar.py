226702900e9caf9100cceea1c87a0023d89ab0dd
PatelVatsalB21 <patelvatsalb21@gmail.com>
2022-05-27 09:51:15 +0530

    Added Load Image function

--- Mission_Interstellar.py
+++ Mission_Interstellar.py
@@ -17,6 +17,21 @@ clock = pygame.time.Clock()
 
 running = True
 
+def load_image(file, size_x=-1, size_y=-1, colorkey=None):
+    path = os.path.join('Sprites', file)
+    image = pygame.image.load(path)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+
+    if size_x != -1 or size_y != -1:
+        image = pygame.transform.scale(image, (size_x, size_y))
+
+    return image, image.get_rect()
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

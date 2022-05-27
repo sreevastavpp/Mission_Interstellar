226355e0deccf54590506d3b918cf08e654cf17d
PatelVatsalB21 <patelvatsalb21@gmail.com>
2022-05-27 19:23:58 +0530

    Added Gravity and Player Explode

--- Mission_Interstellar.py
+++ Mission_Interstellar.py
@@ -89,6 +89,7 @@ class Player(pygame.sprite.Sprite):
 
         (self.image, self.rect) = load_image('ship.png', 40,
                                              40, -1)
+        # self.image = pygame.Surface((40, 40), pygame.SRCALPHA)
         self.original_image = self.image
         self.position = vec(pos)
         self.rect = self.image.get_rect(center=self.position)
@@ -100,9 +101,18 @@ class Player(pygame.sprite.Sprite):
         self.fuel = 0
         self.maxfuel = 0
         self.gameOver = False
+        self.isGravity = False
+        self.explosion_sound = pygame.mixer.Sound('Sprites/explosion.wav')
+        self.explosion_sound.set_volume(0.1)
+        self.flame_up = flame(40,80,self,1)
+        self.flame_down = flame(40,80,self,4)
+        self.flame_right = flame(40,40,self,3)
+        self.flame_left = flame(40,40,self,2)
         self.rect.center = pos
+        # screen.blit(self.image, self.rect)
 
     def rotate(self):
+        # Rotate the acceleration vector.
         self.acceleration.rotate_ip(self.angle_speed)
         self.angle += self.angle_speed
         if self.angle > 360:
@@ -116,12 +126,20 @@ class Player(pygame.sprite.Sprite):
         global gameovermenu
         if self.rect.left > width:
             self.gameOver = True
+            gameovermenu = True
+            game_over()
         if self.rect.right < 0:
             self.gameOver = True
+            gameovermenu = True
+            game_over()
         if self.rect.top > height:
             self.gameOver = True
+            gameovermenu = True
+            game_over()
         if self.rect.bottom < 0:
             self.gameOver = True
+            gameovermenu = True
+            game_over()
 
     def update(self):
         self.checkbounds()
@@ -130,31 +148,146 @@ class Player(pygame.sprite.Sprite):
             self.angle_speed = -1
             self.rotate()
             self.fuel -= 0.01
+            self.flame_left.update(self)
         if keys[K_RIGHT]:
             self.angle_speed = 1
             self.rotate()
             self.fuel -= 0.01
+            self.flame_right.update(self)
         if keys[K_UP]:
             self.vel += self.acceleration
             self.fuel -= 0.05
+            self.flame_up.update(self)
+
         if keys[K_DOWN]:
             self.vel -= self.acceleration
             self.fuel -= 0.05
+            self.flame_down.update(self)
         if keys[K_SPACE]:
             brake_vector = vec(-(self.vel.x * 0.1), -(self.vel.y * 0.1))
             self.vel += brake_vector
             self.fuel -= 0.1
+            self.flame_up.update(self)
+            self.flame_down.update(self)
+            self.flame_right.update(self)
+            self.flame_left.update(self)
         if keys[K_ESCAPE]:
             self.gameOver = True
+            global gameovermenu
+            gameovermenu = True
+            game_over()
 
+        # for event in pygame.event.get():
+        #     if event.type == pygame.KEYUP:
+                # if event.key == pygame.K_UP:
+                #
+                # if event.key == pygame.K_DOWN:
+                # if event.key == pygame.K_LEFT:
+                # if event.key == pygame.K_RIGHT:
+                # if event.key == pygame.K_SPACE:
+
+        # max speed
         if self.vel.length() > MAX_SPEED:
             self.vel.scale_to_length(MAX_SPEED)
 
+        global speed_vec
+        speed_vec = self.vel
         self.position += self.vel
         self.rect.center = self.position
 
         screen.blit(self.image, self.rect)
 
+
+    def explode(self):
+        (x, y) = self.rect.center
+        if pygame.mixer.get_init():
+            self.explosion_sound.play(maxtime=1000)
+
+        self.explosion = Explosion(x, y, 60)
+        for i in range(0,30):
+            self.explosion.update()
+        self.kill()
+        self.explosion.kill()
+        self.gameOver = True
+        global gameovermenu
+        gameovermenu = True
+
+    def gravity(self, planet, gravity_const):
+        # self.vel += self.low_acceleration
+        # if(self.angle_speed>0):
+        #     self.angle_speed = 0.5
+        # else:
+        #     self.angle_speed = -0.5
+        gravity_const = gravity_const
+        self.rotate()
+        dx = self.position.x - planet.rect.centerx
+        dy = self.position.y - planet.rect.centery
+        distance = math.hypot(dx, dy)
+        if distance > 0:
+            dx /= distance
+            dy /= distance
+
+        # distance = vec(self.rect.center).distance_to(planet.rect.center)
+        if planet.radius * 0.5 < distance < planet.radius * gravity_const:
+            angle_btw = vec(self.rect.center).angle_to(planet.rect.center)
+            # print(angle_btw)
+            # print(self.angle)
+            # print(planet.radius*GRAVITY - distance)
+
+            planet_distance = (planet.radius * gravity_const - distance)
+
+            if self.vel.magnitude() < 0.5:
+                angle_constant = gravity_const * 10 * planet_distance
+            else:
+                angle_constant = gravity_const * 20 * self.vel.magnitude() * planet_distance
+            # print(self.vel.magnitude())
+
+            if not 8 < abs(angle_btw) < 10:
+                if planet.rect.left - self.rect.x > planet.rect.right - self.rect.x:
+                    if 230 < self.angle or self.angle < 60:
+                        self.angle_speed = (angle_btw * angle_constant) / planet.radius
+                        # self.vel += self.low_acceleration
+                    else:
+                        self.angle_speed = -(angle_btw * angle_constant) / planet.radius
+                        # self.vel -= self.low_acceleration
+                else:
+                    if 230 < self.angle or self.angle < 60:
+                        self.angle_speed = -(angle_btw * NORMAL_GRAVITY * self.vel.magnitude() * 2) / planet.radius
+                        # self.vel -= self.low_acceleration
+                    else:
+                        self.angle_speed = (angle_btw * NORMAL_GRAVITY * self.vel.magnitude() * 2) / planet.radius
+                        # self.vel += self.low_acceleration
+
+            # self.vel *= 1.009
+            # move_dist = min(self.vel.magnitude(), dist)
+
+            self.vel -= vec(dx, dy) * 0.02
+            # global speed_vec
+            # speed_vec = self.vel
+            # self.position += self.vel
+            # self.rect.center = self.position
+
+            # if self.rect.x - planet.rect.top > self.rect.x - planet.rect.right:
+            #     self.vel += self.low_acceleration
+            # else:
+            #     self.vel -= self.low_acceleration
+
+        # self.isGravity = True
+        # self.rect = self.image.get_rect(center=pos)
+        # self.position = vec(pos)
+        # offset = vec(100, 0)
+        #
+        # self.angle -= 2
+        #
+        # self.rotate()
+        # self.rect.center = self.position + offset.rotate(self.angle)
+
+    def addFuel(self, fuelstep):
+        self.fuel += fuelstep
+        for i in range(1000):
+            i = i
+        if self.fuel > self.maxfuel:
+            self.fuel = self.maxfuel
 
 player = Player(screen.get_rect().center)
 
